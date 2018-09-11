07fd571b0bdcceb7a90e15e67cf600c05d4c77a0
Joseph Harwood <josephblakeharwood@gmail.com>
2018-09-11 21:19:58 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,14 @@
-//define functions here
+function getIt() {
+  $('p').on("click", function(){
+    alert("Hey!")
+  });
+}
+
+function frameIt() {
+  $('.tasty').on("load", function(){
+    alert("Hey!")
+  });
+}
 
 $(document).ready(function(){
 
